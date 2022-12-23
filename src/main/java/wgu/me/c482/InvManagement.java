f4ec37ba3a33d66144fb796ca25b4f82538fb54f
Tanner Mills <tanner.mills2000@gmail.com>
2022-12-23 02:41:24 -0700

    Working loader for main page.

---
+++ src/main/java/wgu/me/c482/InvManagement.java
@@ -0,0 +1,18 @@
+package wgu.me.c482;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+
+public class InvManagement  extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(InvManagement.class.getResource("main-form.fxml"));
+        Scene scene = new Scene(loader.load(), 900, 500);
+        stage.setTitle("Home Page");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
