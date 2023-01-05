215fa730e06e5c68fac2a18586260e05dfa7d118
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-04 20:09:07 -0700

    altered machineId to be in-line with the UML requirements

--- src/main/java/wgu/me/c482/InHouse.java
+++ src/main/java/wgu/me/c482/InHouse.java
@@ -1,7 +1,7 @@
 package wgu.me.c482;
 
 public class InHouse extends Part {
-    public int machineId;
+    private int machineId;
     public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
         this.machineId = machineId;
