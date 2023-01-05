66c03fbddca1d62e6beb5def6490079561caeb44
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-04 20:00:45 -0700

    Added the setter and getter as specified in the UML

--- src/main/java/wgu/me/c482/InHouse.java
+++ src/main/java/wgu/me/c482/InHouse.java
@@ -1,7 +1,25 @@
 package wgu.me.c482;
 
 public class InHouse extends Part {
-    public InHouse(int id, String name, double price, int stock, int min, int max) {
+    public int machineId;
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    /**
+     *
+     * @param machineId the machineId to set for the Part
+     */
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
+
+    /**
+     *
+     * @return the machineId of the InHouse Part
+     */
+    public int getMachineId() {
+        return this.machineId;
     }
 }
