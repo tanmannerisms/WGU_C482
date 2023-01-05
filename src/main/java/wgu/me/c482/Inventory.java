07463d17cdb0b960c86ff16a882237f569d590d2
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-05 06:21:59 -0700

    Added the Inventory.java class and created all the instance methods and variables required in the UML

---
+++ src/main/java/wgu/me/c482/Inventory.java
@@ -0,0 +1,57 @@
+package wgu.me.c482;
+
+import javafx.collections.ObservableList;
+
+import java.util.*;
+
+public class Inventory {
+    private ObservableList allParts;
+    private ObservableList allProducts;
+
+    public void addPart(Part part) {
+
+    }
+
+    public void addProduct(Product product) {
+
+    }
+
+    public Part lookupPart(int partId) {
+
+    }
+
+    public Product lookupProduct(int productId) {
+
+    }
+
+    public ObservableList<Part> lookupPart(String partName) {
+
+    }
+
+    public ObservableList<Product> lookupProduct(String productName) {
+
+    }
+
+    public void updatePart(int index, Part selectedPart) {
+
+    }
+
+    public void updateProduct(int index, Product newProduct) {
+
+    }
+
+    public boolean deletePart(Part selectedPart) {
+
+    }
+
+    public boolean deleteProduct(Product selectedProduct) {
+
+    }
+
+    public ObservableList<Part> getAllParts() {
+
+    }
+    public ObservableList<Product> getAllProducts() {
+
+    }
+}
