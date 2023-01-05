32186875588f987ac66d1ab0756f370d1b8aa6b6
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-05 06:31:24 -0700

    Added the Product.java class and created all the instance methods and variables required in the UML

---
+++ src/main/java/wgu/me/c482/Product.java
@@ -0,0 +1,82 @@
+package wgu.me.c482;
+
+import javafx.collections.ObservableList;
+
+public class Product {
+    private ObservableList<Part> associatedParts;
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private int min;
+    private int max;
+
+    public Product(int id, String name, double price, int stock, int min, int max) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getId() {
+        return this.id
+    }
+
+    public String getName() {
+        return this.name
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+
+    public int getMax() {
+        return this.max;
+    }
+
+    public void addAssociatedPart(Part part) {
+
+    }
+
+    public boolean deleteAssociatedPart(Part selectedAssociatedPart) {
+
+    }
+
+    public ObservableList<Part> getAllAssociatedParts() {
+
+    }
+}
