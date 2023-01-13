640d40c739ba2279158b7f8603bbaea67dde75e0
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-13 12:49:36 -0700

    Removed unnecessary BS from InvManagement.java

--- src/main/java/wgu/me/c482/InvManagement.java
+++ src/main/java/wgu/me/c482/InvManagement.java
@@ -12,37 +12,13 @@ import java.net.URL;
 import javafx.fxml.FXMLLoader;
 
 public class InvManagement extends Application {
-    private Stage mainStage;
-    public Stage secondaryStage;
-    FXMLLoader addPartLoader = new FXMLLoader(InvManagement.class.getResource("add-part.fxml"));
-
     @Override
     public void start(Stage stage) throws Exception {
-        this.mainStage = stage;
         FXMLLoader mainForm = new FXMLLoader(InvManagement.class.getResource("main-form.fxml"));
         Scene mainScene = new Scene(mainForm.load(), 900, 500);
-        mainStage.setTitle("Home Page");
-        mainStage.setScene(mainScene);
-        mainStage.setResizable(false);
-        mainStage.show();
-    }
-
-    @FXML
-    public void switchScene() {
-        try {
-            secondaryStage = new Stage();
-            Scene addPartScene = new Scene(addPartLoader.load());
-            secondaryStage.setScene(addPartScene);
-            secondaryStage.sizeToScene();
-            secondaryStage.setResizable(false);
-            secondaryStage.showAndWait();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    public void exit() {
-        mainStage.close();
+        stage.setTitle("Home Page");
+        stage.setScene(mainScene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
