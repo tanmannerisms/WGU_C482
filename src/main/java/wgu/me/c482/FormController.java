57350a4d8f7d414fa5c959d2bf37e7832ef60aa5
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-12 22:19:48 -0700

    got the addPart button working

--- src/main/java/wgu/me/c482/FormController.java
+++ src/main/java/wgu/me/c482/FormController.java
@@ -8,17 +8,4 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class FormController {
-    @FXML
-    private Button addProduct;
-    private Button changeProduct;
-    private Button deleteProduct;
-
-    public Scene newScene(String form, int width, int height) {
-        return null;
-    }
-    @FXML
-    protected void addProduct() throws IOException {
-
-        InvManagement.switchScene(addProduct);
-    }
 }
--- src/main/java/wgu/me/c482/InvManagement.java
+++ src/main/java/wgu/me/c482/InvManagement.java
@@ -1,29 +1,48 @@
 package wgu.me.c482;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+
 import javafx.fxml.FXMLLoader;
 
 public class InvManagement extends Application {
-    public Stage mainStage;
-    public Scene addPartScene;
-    public Scene addProductScene;
+    private Stage mainStage;
+    public Stage secondaryStage;
+    FXMLLoader addPartLoader = new FXMLLoader(InvManagement.class.getResource("add-part.fxml"));
+
     @Override
     public void start(Stage mainStage) throws Exception {
+        this.mainStage = mainStage;
         FXMLLoader mainForm = new FXMLLoader(InvManagement.class.getResource("main-form.fxml"));
         Scene mainScene = new Scene(mainForm.load(), 900, 500);
-        mainStage.setTitle("Home Page");
-        mainStage.setScene(mainScene);
-        mainStage.setResizable(false);
-        mainStage.show();
-
-
+        this.mainStage.setTitle("Home Page");
+        this.mainStage.setScene(mainScene);
+        this.mainStage.setResizable(false);
+        this.mainStage.show();
     }
 
-    public void switchScene(Scene nextScene) {
-        mainStage.setScene(nextScene);
-        mainStage.show();
+    @FXML
+    public void switchScene() {
+        try {
+            secondaryStage = new Stage();
+            Scene addPartScene = new Scene(addPartLoader.load());
+            secondaryStage.setScene(addPartScene);
+            secondaryStage.sizeToScene();
+            secondaryStage.setResizable(false);
+            secondaryStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void exit() {
+        this.mainStage.close();
     }
 }
