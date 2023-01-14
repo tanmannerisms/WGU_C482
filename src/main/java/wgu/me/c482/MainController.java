efefbad809670bbce10d3b511b44fc490941209f
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-13 17:30:39 -0700

    Added ProductController.java to the mix and added an addProductSceneChange() method to the MainController class

--- src/main/java/wgu/me/c482/MainController.java
+++ src/main/java/wgu/me/c482/MainController.java
@@ -6,10 +6,16 @@ import javafx.stage.Stage;
 
 public class MainController extends Controller{
     private static String addPartXml = "add-part.fxml";
+    private static String addProductXml = "add-product.fxml";
 
     @FXML
     public void addPartSceneChange() {
         newWindow(addPartXml);
+    }
+
+    @FXML
+    public void addProductSceneChange() {
+        newWindow(addProductXml);
     }
     @Override
     void exit() {
---
+++ src/main/java/wgu/me/c482/ProjectController.java
@@ -0,0 +1,13 @@
+package wgu.me.c482;
+
+public class ProjectController extends Controller{
+
+    @Override
+    void exit() {
+
+    }
+    @Override
+    void cancel() {
+
+    }
+}
