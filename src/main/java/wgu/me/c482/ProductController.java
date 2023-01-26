7fa8b33795a375983767d4423c2947a9da993711
tmills2000 <tanner.mills2000@gmail.com>
2023-01-26 16:28:23 -0700

    Initial of ProductController method for adding Products.

--- src/main/java/wgu/me/c482/ProductController.java
+++ src/main/java/wgu/me/c482/ProductController.java
@@ -1,10 +1,27 @@
 package wgu.me.c482;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ProductController extends Controller{
+    private String partName;
+    private int partInventory, partMin, partMax;
+    private double partPrice;
+    private TextField idField, nameField, inventoryField, priceField, minField, maxField,
     public ProductController() {
         super();
     }
     ProductController(Inventory inventory) {
         super(inventory);
+    }
+    @FXML
+    private void addProduct() {
+        partName = nameField.getText();
+        partInventory = getIntFromTextField(inventoryField);
+        partPrice = getDoubleFromTextField(priceField);
+        partMin = getIntFromTextField(minField);
+        partMax = getIntFromTextField(maxField);
+        Product newProduct = new Product(productId, productName, productPrice, productInventory, productMin, productMax);
+        System.out.println("Part " + newProduct.getName() + " has been successfully created.");
     }
 }
