f1af9ad9d229c20775faf1c65e7a835494e77848
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-28 17:15:13 -0700

    Code cleanup and addition of fxIds for the TableViews

--- src/main/java/wgu/me/c482/MainController.java
+++ src/main/java/wgu/me/c482/MainController.java
@@ -1,12 +1,16 @@
 package wgu.me.c482;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController extends Controller implements Initializable {
+    @FXML
+    private TableView productsTable, partsTable;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -15,23 +19,27 @@ public class MainController extends Controller implements Initializable {
         super();
     }
     @FXML
-    private void addProductSceneChange() {
+    private void addProductSceneChange(ActionEvent actionEvent) {
         Window addProduct = new Window("add-product.fxml", "Add-Product");
         addProduct.showWindow();
+        actionEvent.consume();
     }
     @FXML
-    private void changeProductSceneChange() {
+    private void changeProductSceneChange(ActionEvent actionEvent) {
         Window changeProduct = new Window("modify-product.fxml", "Change Product");
         changeProduct.showWindow();
+        actionEvent.consume();
     }
     @FXML
-    private void addPartSceneChange() {
+    private void addPartSceneChange(ActionEvent actionEvent) {
         Window addPart = new Window("add-part.fxml", "Add-Part");
         addPart.showWindow();
+        actionEvent.consume();
     }
     @FXML
-    private void changePartSceneChange() {
+    private void changePartSceneChange(ActionEvent actionEvent) {
         Window changePart = new Window("modify-part.fxml", "Alter Part");
         changePart.showWindow();
+        actionEvent.consume();
     }
 }
