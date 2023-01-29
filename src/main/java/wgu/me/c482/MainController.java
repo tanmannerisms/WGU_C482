4c0b5d94a879030157a5d876e09385f8f226c659
Tanner Mills <tanner.mills2000@gmail.com>
2023-01-28 17:47:17 -0700

    Table views are populating with data.

--- src/main/java/wgu/me/c482/MainController.java
+++ src/main/java/wgu/me/c482/MainController.java
@@ -3,7 +3,9 @@ package wgu.me.c482;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +13,12 @@ import java.util.ResourceBundle;
 public class MainController extends Controller implements Initializable {
     @FXML
     private TableView productsTable, partsTable;
+    @FXML
+    private TableColumn<Part, Integer> partIdColumn, partStockColumn, productIdColumn, productStockColumn;
+    @FXML
+    private TableColumn<Part, String> partNameColumn, productNameColumn;
+    @FXML
+    private TableColumn<Part, Double> partPriceColumn, productPriceColumn;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         updateTables();
@@ -44,7 +52,17 @@ public class MainController extends Controller implements Initializable {
     }
     @FXML
     private void updateTables() {
-        productsTable.setItems(Inventory.getAllProducts());
         partsTable.setItems(Inventory.getAllParts());
+        partIdColumn.setCellValueFactory(new PropertyValueFactory<Part, Integer>("id"));
+        partNameColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("name"));
+        partStockColumn.setCellValueFactory(new PropertyValueFactory<Part, Integer>("stock"));
+        partPriceColumn.setCellValueFactory(new PropertyValueFactory<Part, Double>("price"));
+
+        productsTable.setItems(Inventory.getAllProducts());
+        productIdColumn.setCellValueFactory(new PropertyValueFactory<Part, Integer>("id"));
+        productNameColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("name"));
+        productStockColumn.setCellValueFactory(new PropertyValueFactory<Part, Integer>("stock"));
+        productPriceColumn.setCellValueFactory(new PropertyValueFactory<Part, Double>("price"));
+
     }
 }
