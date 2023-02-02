5b67e07f968d6d94986b5c778e2c0efbf80c6e70
tmills2000 <tanner.mills2000@gmail.com>
2023-02-02 11:31:21 -0700

    Final javadoc and minor alteration of createTestData method.

--- src/main/java/wgu/me/c482/InvManagement.java
+++ src/main/java/wgu/me/c482/InvManagement.java
@@ -5,6 +5,12 @@ import javafx.stage.Stage;
 
 
 public class InvManagement extends Application {
+    /**
+     * Creates a window with the main form and creates the test data.
+     *
+     * @param stage not used
+     * @throws Exception not thrown.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         createTestData();
@@ -12,12 +18,16 @@ public class InvManagement extends Application {
         Window mainWindow = new Window("main-form.fxml", "Home");
         mainWindow.showWindow();
     }
+
+    /**
+     * Creates two sample Parts and one sample Product to use in testing.
+     */
     private void createTestData() {
-        Part sample0 = new InHouse(1, "SamplePart0", 10.00, 5, 0, 10, 1245);
-        Part sample1 = new Outsourced(2, "SamplePart1", 0.99, 5, 0, 10, "Pseudo Company");
+        Part sample0 = new InHouse(Inventory.partIdIterator, "SamplePart0", 10.00, 5, 0, 10, 1245);
         Inventory.addPart(sample0);
+        Part sample1 = new Outsourced(Inventory.partIdIterator, "SamplePart1", 0.99, 5, 0, 10, "Pseudo Company");
         Inventory.addPart(sample1);
-        Product sample10 = new Product(1, "SampleProduct", 10.99, 1, 0, 100);
+        Product sample10 = new Product(Inventory.productIdIterator, "SampleProduct", 10.99, 1, 0, 100);
         Inventory.addProduct(sample10);
     }
 }
